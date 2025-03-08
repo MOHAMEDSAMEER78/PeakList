e4d12d20d8e4c5a223cdc2499b5f522f6f7f8d60
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2025-03-08 11:20:09 +0530

    Initial commit

---
+++ src/test/java/com/example/PeakList/PeakListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PeakList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
