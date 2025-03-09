e0819389b824037d04612a48da826ed16e2260ef
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2025-03-09 09:13:30 +0530

    model schema creation

--- src/main/java/com/example/PeakList/model/Notes.java
+++ src/main/java/com/example/PeakList/model/Notes.java
@@ -1,8 +1,17 @@
 package com.example.PeakList.model;
 
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+
+@Data
+@Entity
+@Table(name = "notes")
 public class Notes {
+    @Id
     private int noteId;
     private int userId;
     private String title;
--- src/main/java/com/example/PeakList/model/User.java
+++ src/main/java/com/example/PeakList/model/User.java
@@ -1,8 +1,17 @@
 package com.example.PeakList.model;
 
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     private int userId;
     private String username;
     private String password;
